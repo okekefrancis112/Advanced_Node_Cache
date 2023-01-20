27da758fc1c5a92e919c718138917c750522f534
Dev_francis <okeke98@gmail.com>
2023-01-20 09:32:52 +0100

    refactored test script

--- tests/header.test.js
+++ tests/header.test.js
@@ -24,5 +24,34 @@ test('clicking login starts oauth flow', async () => {
     await page.click('.right a')
     const url = await page.url();
 
-    console.log(url);
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+    const id = '63c511e60587b2e6d903444c';
+
+    const Buffer = require("safe-buffer").Buffer;
+    const sessionObject = {
+        passport: {
+            user: id
+        }
+    }
+    const sessionString = Buffer.from(
+        JSON.stringify(sessionObject)
+    ).toString('base64');
+
+    const Keygrip = require('keygrip');
+    const keys = require('../config/keys');
+    const keygrip = new Keygrip([keys.cookieKey]);
+    const sig = keygrip.sign('session=' + sessionString);
+
+    console.log(sessionString, sig);
+    await page.setCookie({ name: 'session', value: sessionString });
+    await page.setCookie({ name: 'session.sig', value: sig });
+    await page.goto('localhost:3000');
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+
+    expect(text).toEqual('Logout');
 });
