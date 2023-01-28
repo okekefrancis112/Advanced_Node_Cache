b82f3c3ffd6cdb2d94d6634c663679c381b6362e
Dev_francis <okeke98@gmail.com>
2023-01-28 14:26:23 +0100

    adding image file

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -47,11 +47,20 @@ class BlogFormReview extends Component {
     submitBlog(formValues, history);
   }
 
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
+  }
+
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add an Image</h5>
+        <input
+        onChange={this.onFileChange.bind(this)}
+        type="file" accept='image/*'/>
 
         {this.renderButtons()}
       </form>
