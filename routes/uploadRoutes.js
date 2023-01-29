82b00979c7c7e437f09b08cb1ba813f2afefbf0e
Dev_francis <okeke98@gmail.com>
2023-01-29 01:07:32 +0100

    upload route added

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/keys');
+
+
+const s3 = new AWS.S3({
+    accessKeyId: Keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey
+})
+
+
+module.exports = app => {
+    app.get('/api/upload', (req, res) => {
+        const key = `${req.User.id}/${uuid().jpeg}`
+
+        s3.getSignedUrl('putObject', {
+            Bucket: 'my-blog-bucket-123',
+            ContentType: 'jpeg',
+            key: key
+        },
+        (err, url) => res.send({ key, url }))
+    });
+};
